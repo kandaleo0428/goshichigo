0365e132e73a9467cc84ac932d035d3fae8a815c
kandaleo <kandaleo6969@gmail.com>
2020-10-15 13:50:34 +0900

    編集機能、削除機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,4 +21,24 @@ class PostsController < ApplicationController
     redirect_to("/posts/index")
   end
 
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post_by(id: params[:id])
+    @post.content_one = params[:content_one]
+    @post.content2 = params[:content2]
+    @post.content3 = params[:content3]
+    @post.message = params[:message]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
+
 end
