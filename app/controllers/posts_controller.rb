c0408fc8b931bd042e6ca9153e008e4279b7f133
kandaleo0428 <kandaleo6969@gmail.com>
2020-10-17 08:27:33 +0900

    一句詠む際に4つの別データ（初句・二句・結句・メッセージ）をまとめて１つのデータで送るテストに成功

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,10 +13,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content1: params[:content1])
-    @post = Post.new(content2: params[:content2])
-    @post = Post.new(content3: params[:content3])
-    @post = Post.new(message: params[:message])
+    @post = Post.new(content1: params[:content1],content2: params[:content2],content3: params[:content3],message: params[:message])
+    #@post = Post.new(content2: params[:content2])
+    #@post = Post.new(content3: params[:content3])
+    #@post = Post.new(message: params[:message])
     
     if @post.save
     flash[:notice] ="一句、詠みました（投稿しました）"
