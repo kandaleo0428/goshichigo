461864854fae5e64928081b6ac1d10d13dc6ff38
kandaleo0428 <kandaleo6969@gmail.com>
2020-11-02 12:04:35 +0900

    ’’コメント機能の作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new(comment:params[:comment], user_id:@current_user.id, post_id:params[:post_id])
+    @comment = Comment.new(comment:params[:comment], user_id:@current_user.id)
       if @comment.save
         flash[:notice] = "コメントしました"
-        redirect_to("/posts/:id")
+        redirect_to("/posts/#{params[:post_id]}")
       else
         flash[:notice] = "コメントできませんでした"
-        render("/posts/show")
+        render("/posts/#{params[:post_id]}")
       end
   end
 
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
     @comment = Comment.find_by(id: params[:id])
     flash[:notice] = "コメントを削除しました"
     @comment.destroy
-    redirect_to("/posts/show")
+    redirect_to("/posts/#{params[:post_id]}")
   end
   
 end
