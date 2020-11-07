423fe4112f6ea85bf64a718ec6387135a5d63b51
kandaleo0428 <kandaleo6969@gmail.com>
2020-11-07 09:17:48 +0900

    時刻を標準時間（UTC）から日本時間（JST）に変更する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,6 +68,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find_by(id: params[:id]) 
     @post.destroy
+    
     flash[:notice] = "一句、削除しました（投稿を削除しました）"
     redirect_to("/posts/index")
   end
