6bc240de3ef77a6211d5c321f895898578e5d9de
kandaleo0428 <kandaleo6969@gmail.com>
2020-11-18 10:14:54 +0900

    「句の詳細」コメント欄を修正

--- app/assets/stylesheets/application.js
+++ app/assets/stylesheets/application.js
@@ -1,7 +1,8 @@
 //= require jquery3
+//= require jquery
 //= require popper
 //= require bootstrap-sprockets
 
 $(function(){
-    setTimeout("$('.time-limit').fadeOut('slow')",1000)
+    setTimeout("$('.time-limit').fadeOut('slow')", 1000) 
 })
